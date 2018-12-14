ef40a776f534c12937eb915a402fa130b549932d
Dani Deicide <me@danielturus.us>
2018-12-14 13:56:52 +0200

    Second commit

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+import './assets/css/main.scss'
+export default {
+  
+}
+</script>
